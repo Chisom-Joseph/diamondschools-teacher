12480c144d1e209c547faf3412d6b5cca73627f1
njokuchisomjoseph@gmail.com <njokuchisomjoseph@gmail.com>
2025-01-14 22:13:07 +0100

    Added index route

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,18 @@
 const router = require("express").Router();
 
+const loginVerifier = require("../middlewares/loginVerifire");
+
+router.use(require("../middlewares/setCurrentPath"));
+router.use("/dashboard", loginVerifier, require("./dashboard"));
+router.use("/auth", loginVerifier, require("./auth"));
+router.use("/api", loginVerifier, require("./api"));
+
+router.get("/", (req, res) => {
+  res.redirect("/dashboard/dashboard");
+});
+
+router.get("*", (req, res) => {
+  res.render("error.ejs");
+});
+
 module.exports = router;
