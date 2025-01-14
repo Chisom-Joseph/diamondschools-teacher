6c536962320b0d67eb9a54269c73e8912a7e91b5
njokuchisomjoseph@gmail.com <njokuchisomjoseph@gmail.com>
2025-01-14 22:15:07 +0100

    Added middlewares

---
+++ middlewares/studentProfileImageUpload.js
@@ -0,0 +1,36 @@
+const multer = require("multer");
+const path = require("path");
+
+// Set storage engine
+const storage = multer.memoryStorage({
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+// Initialize upload
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 2000000 },
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+}).single("profilePhoto");
+
+// Check File Type
+function checkFileType(file, cb) {
+  const filetypes = /jpeg|jpg|png/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb("Error: Images Only!");
+  }
+}
+
+module.exports = upload;
