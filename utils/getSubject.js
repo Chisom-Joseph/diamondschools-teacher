bb385dbf2b39669cdb908e84a0eb3540d5d633d1
njokuchisomjoseph@gmail.com <njokuchisomjoseph@gmail.com>
2025-01-15 11:21:11 +0100

    Added get subject util

---
+++ utils/getSubject.js
@@ -0,0 +1,36 @@
+const { Subject, Class } = require("../models");
+
+module.exports = async (id) => {
+  try {
+    if (!id) return {};
+
+    const subjectFromDb = await Subject.findOne({
+      include: [
+        {
+          model: Class,
+          attibutes: ["id", "name"],
+        },
+      ],
+      attibutes: ["id", "name"],
+      where: { id },
+    });
+
+    if (!subjectFromDb) return {};
+
+    const subject = subjectFromDb.dataValues;
+
+    subject.class = {};
+    if (subject.Class) {
+      subject.class = subject.Class.dataValues;
+    }
+    delete subject.Class;
+    delete subject.ClassId;
+
+    console.log(await subject);
+    return subject;
+  } catch (error) {
+    console.log("ERROR GETTING SUBJECT");
+    console.log(error);
+    return {};
+  }
+};
