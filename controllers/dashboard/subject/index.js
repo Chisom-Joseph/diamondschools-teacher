8f81f2ef43992ca0dce36af69e07357aaaa6e65b
njokuchisomjoseph@gmail.com <njokuchisomjoseph@gmail.com>
2025-01-16 06:43:42 +0100

    Added index controller for subject

---
+++ controllers/dashboard/subject/index.js
@@ -0,0 +1,24 @@
+module.exports = (req, res) => {
+  switch (req.query.action) {
+    case "add":
+      require("./add")(req, res);
+      break;
+    case "update":
+      require("./update")(req, res);
+      break;
+    case "delete":
+      require("./delete")(req, res);
+      break;
+    default:
+      req.flash("alert", {
+        status: "error",
+        section: "update",
+        message: "Something went wrong",
+      });
+      req.flash("form", req.body);
+      req.flash("formSection", "add");
+      req.flash("status", 400);
+      return res.redirect("/dashboard/class");
+      break;
+  }
+};
