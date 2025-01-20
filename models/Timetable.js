c0e831d4f60602cd8223e9be40231d2d2bd5a263
njokuchisomjoseph@gmail.com <njokuchisomjoseph@gmail.com>
2025-01-20 08:28:39 +0100

    Updated Timetable db

--- models/Timetable.js
+++ models/Timetable.js
@@ -1,35 +1,46 @@
 module.exports = (sequelize, DataTypes) => {
-  const Timetable = sequelize.define("Timetable", {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      allowNull: false,
-      primaryKey: true,
-      validate: {
-        notEmpty: true,
+  const Timetable = sequelize.define(
+    "Timetable",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+        validate: {
+          notEmpty: true,
+        },
+      },
+      day: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+        },
+      },
+      time: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+        },
+      },
+      subject: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        validate: {
+          notEmpty: true,
+        },
       },
     },
-    day: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: true,
-      },
-    },
-    time: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: true,
-      },
-    },
-    subject: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: true,
-      },
-    },
-  });
+    {
+      indexes: [
+        {
+          unique: true,
+          fields: ["day", "time"],
+        },
+      ],
+    }
+  );
   return Timetable;
 };
